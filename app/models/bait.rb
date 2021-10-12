f904c7e44c880cd32e79bf7e6c4d34ea874b4b04
cesar madera <camgcamg11@gmail.com>
2021-10-12 10:44:44 -0500

    added some validations and associations

--- app/models/bait.rb
+++ app/models/bait.rb
@@ -1,2 +1,6 @@
 class Bait < ApplicationRecord
+  #validates :cost, :name, :description, presence: true
+  has_many :bait_catches
+  has_many :catches, through: :bait_catches
+  #scope :spots, ->{ self.catch.spots }
 end
--- app/models/catch.rb
+++ app/models/catch.rb
@@ -1,2 +1,4 @@
 class Catch < ApplicationRecord
+  has_many :catch_spots
+  has_many :spots, through: :catch_spots
 end
