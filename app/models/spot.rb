d01eda106a4b4effbc4ef9a836a1c50e3762c1ad
Felipe Zingoni <felipezingoni.fz@gmail.com>
2021-10-12 14:05:16 -0300

    model spot

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,8 @@
 class Spot < ApplicationRecord
+  has_many_attached :images
+  has_many :catch_spots
+  has_many :voted_spots
+  has_many :posts
+  validates :name, :description, :longitude, :latitude, presence: true
+  # reverse_geocoded_by :latitude, :longitude, address: :loc
 end
