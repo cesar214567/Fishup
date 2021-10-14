e672e085104100da266517f8694d62dbba407f5a
Eduardo Serrano <eduardoserrano8591@gmail.com>
2021-10-14 12:01:35 -0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :spots, only: %i[index show new create edit update] do
-    resources :catches, only: %i[index]
-  end
-  resources :catches, only: %i[show]
+  resources :spots, only: %i[index show new create edit update]
+  #  resources :catches, only: %i[index]
+
+  resources :catches, only: %i[show index]
   resources :baits
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
