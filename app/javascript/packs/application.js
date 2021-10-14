92f23b7edc27292fcf57f52dd7502bdc1c9fb0bb
Eduardo Serrano <eduardoserrano8591@gmail.com>
2021-10-14 12:25:28 -0500

    adds front end

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
-
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
@@ -22,42 +19,38 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
-import { initMapbox } from '../plugins/init_mapbox';
-import { initMapbox1 } from "../plugins/spot_mapbox";
+import {initMapbox} from "../plugins/init_mapbox";
+import {initMapbox1} from "../plugins/spot_mapbox";
 
-
-
-document.addEventListener('turbolinks:load', () => {
-  initMapbox1();
-  initMapbox();
+document.addEventListener("turbolinks:load", () => {
+	initMapbox1();
+	initMapbox();
 });
 const cities = document.getElementById("user_city");
 const countries = document.getElementById("user_country");
-if (countries){
-  countries.addEventListener('change', (event) => {
-    cities.innerHTML = "";
-    const actual_country = countries.options[countries.selectedIndex];
-    fetch('https://countriesnow.space/api/v0.1/countries/cities',
-      {
-        method: "POST",
-        body: JSON.stringify({"country": actual_country.text }),
-        headers: {
-          "X-Powered-By": "Express",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "*",
-          "Content-Type": "application/json",
-          "Content-Length": 858,
-          "ETag": "W/35a-V15oVpwHN0jBGP5cr5uJtRIWeW4",
-          "Date": Date.now(),
-          "Connection": "keep-alive"
-        }
-      })
-      .then(response => response.json())
-      .then(response => {
-        response.data.forEach(element => {
-          cities.insertAdjacentHTML("beforeend",`<option value="${element}">${element}</option>`)
-        })
-
-      })
-  });
+if (countries) {
+	countries.addEventListener("change", (event) => {
+		cities.innerHTML = "";
+		const actual_country = countries.options[countries.selectedIndex];
+		fetch("https://countriesnow.space/api/v0.1/countries/cities", {
+			method: "POST",
+			body: JSON.stringify({country: actual_country.text}),
+			headers: {
+				"X-Powered-By": "Express",
+				"Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Headers": "*",
+				"Content-Type": "application/json",
+				"Content-Length": 858,
+				ETag: "W/35a-V15oVpwHN0jBGP5cr5uJtRIWeW4",
+				Date: Date.now(),
+				Connection: "keep-alive",
+			},
+		})
+			.then((response) => response.json())
+			.then((response) => {
+				response.data.forEach((element) => {
+					cities.insertAdjacentHTML("beforeend", `<option value="${element}">${element}</option>`);
+				});
+			});
+	});
 }
