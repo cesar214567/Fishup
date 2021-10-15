54cc6170a7853e414078a101babbbf067b7f3f35
Eduardo Serrano <eduardoserrano8591@gmail.com>
2021-10-15 15:11:42 -0500

    baits searchbar

--- app/models/bait.rb
+++ app/models/bait.rb
@@ -4,4 +4,12 @@ class Bait < ApplicationRecord
   scope :from_spot_id, ->(id) { distinct.joins(bait_catches: { catch: { catch_spots: :spot } }).where(spot: { id: id })}
   validates :cost, :name, :description, presence: true
   has_one_attached :image
+  include PgSearch::Model
+  pg_search_scope :general_search,
+  against: [:name, :description],
+  associated_against: {
+    catches: [:name]
+  }, using: {
+    tsearch: {prefix: true}
+  }
 end
