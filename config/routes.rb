b45d44161eedfeccba74fed28a221b16f867a723
cesar madera <camgcamg11@gmail.com>
2021-10-18 11:46:44 -0500

    verify fish

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :spots, only: %i[index show new create edit update]
   #  resources :catches, only: %i[index]
-
+  resources :catch_spots, only: [] do
+    resources :verify_catch_spots, only: :create
+  end
   resources :catches, only: %i[show index]
   resources :baits
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
