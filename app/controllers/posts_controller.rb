37fc53530994e7e1a5a94c85b55f30ec170ff95a
Eduardo Serrano <eduardoserrano8591@gmail.com>
2021-10-18 11:45:26 -0500

    creates policy for post and new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   before_action :new_post, only: %i[new create]
   def new
+    @spot = Spot.find(params[:spot_id])
+    authorize @post
   end
 
   def create
@@ -13,8 +15,10 @@ class PostsController < ApplicationController
 
   def set_post
     @post = Post.find(params[:id])
+    @post.user = current_user
   end
 
   def post_params
     params.require(:post).permit(:title, :body)
+  end
 end
