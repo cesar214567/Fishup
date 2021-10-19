832b1ec6b460a32e198c57b206f0e80cfd7237c6
Felipe Zingoni <felipezingoni.fz@gmail.com>
2021-10-19 16:50:39 -0300

    Cambios en el javascript de post

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,67 +19,13 @@ ActiveStorage.start();
 
 // External imports
 import "bootstrap";
+import { initCarrousel} from "../plugins/init_carrousel";
+import { initCityCountry } from "../plugins/init_city&country";
 import {initMapbox} from "../plugins/init_mapbox";
 import {initMapbox1} from "../plugins/spot_mapbox";
 document.addEventListener("turbolinks:load", () => {
 	initMapbox1();
 	initMapbox();
+  initCarrousel();
+  initCityCountry();
 });
-const cities = document.getElementById("user_city");
-const countries = document.getElementById("user_country");
-if (countries) {
-	countries.addEventListener("change", (event) => {
-		cities.innerHTML = "";
-		const actual_country = countries.options[countries.selectedIndex];
-		fetch("https://countriesnow.space/api/v0.1/countries/cities", {
-			method: "POST",
-			body: JSON.stringify({country: actual_country.text}),
-			headers: {
-				"X-Powered-By": "Express",
-				"Access-Control-Allow-Origin": "*",
-				"Access-Control-Allow-Headers": "*",
-				"Content-Type": "application/json",
-				"Content-Length": 858,
-				ETag: "W/35a-V15oVpwHN0jBGP5cr5uJtRIWeW4",
-				Date: Date.now(),
-				Connection: "keep-alive",
-			},
-		})
-			.then((response) => response.json())
-			.then((response) => {
-				response.data.forEach((element) => {
-					cities.insertAdjacentHTML("beforeend", `<option value="${element}">${element}</option>`);
-				});
-			});
-	});
-}
-
-
-
-const grande = document.querySelector('.grande')
-const punto = document.querySelectorAll('.punto')
-
-
-// Recorrer TODOS los punto
-punto.forEach((cadaPunto, i) => {
-  // Asignamos un CLICK a cadaPunto
-  punto[i].addEventListener('click', () => {
-
-    // Guardar la posición de ese PUNTO
-    let posicion = i
-    // Calculando el espacio que debe DESPLAZARSE el GRANDE
-    let operacion = posicion * -50
-
-    // MOVEMOS el grand
-    grande.style.transform = `translateX(${operacion}%)`
-
-    // Recorremos TODOS los punto
-    punto.forEach((cadaPunto, i) => {
-      // Quitamos la clase ACTIVO a TODOS los punto
-      punto[i].classList.remove('activo')
-    })
-    // Añadir la clase activo en el punto que hemos hecho CLICK
-    punto[i].classList.add('activo')
-
-  })
-})
